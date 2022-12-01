353177943ae8878369b116c7a3f1eb5e9ca7f3e5
ma1abakiev <malabakievramzan@gmail.com>
2022-12-02 00:06:40 +0600

    last change js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,54 @@
 $(document).ready(function () {
   // Header
-  let topPos = 0
+  let topPos = $(window).scrollTop()
 
+  checkScroll(topPos)
   $(window).on('scroll', function () {
     topPos = $(this).scrollTop()
+    checkScroll(topPos)
+
+  })
+  function checkScroll() {
     topPos > 0
       ? $('.header').addClass('header_active')
       : $('.header').removeClass('header_active')
+  }
+  
+  
+  // Adaptiv
+  $(document).on('click', '.mob', function () {
+    $('.page').animate({width:"toggle"},"fast")
+    $('.adaptiv').animate({width:"toggle"})
+  })
+
+  $(document).on('click', '.adaptiv__header-x', function () {
+    $('.adaptiv').animate({width: "toggle"},"fast" )
+    $('.page').show(200)
   })
 
   // Footer Accordeon
-  $(document).on('click', '.footer-top__title', function () {
+  
+
+  $(document).on('click', '.footer-top__item', function () {
     $('.footer-top__ul:visible').hide(400)
-    $(this).prev().toggleClass('rotate')
-    if ($(this).next().is(':visible')) {
-      $(this).next().hide(400)
-    } else if ($(this).next().is(':hidden')) {
-      $(this).next().slideToggle()
+    $(this).children('.footer-top__arrow-block').children('.footer-top__arrow').toggleClass('rotate')
+    if ($(this).children('.footer-top__ul').is(':visible')) {
+      $(this).children('.footer-top__ul').hide(400)
+    } else if ($(this).children('.footer-top__ul').is(':hidden')) {
+      $(this).children('.footer-top__ul').slideToggle()
     }
   })
 
-  // Adaptiv
-  $(document).on('click', '.mob', function () {
-    $('.page').hide(500)
-    $('.adaptiv').show(500)
+  // Help
+
+  $(document).on('click', '.wa-help-icon', function() {
+    $('.wa-block').addClass('wa-show')
+  })
+  $(document).on('click', '.wa-close-icon', function() {
+    $('.wa-block').removeClass('wa-show')
+  })
+  $(document).on('click', '.wa-whatsapp-icon', function() {
+    $('.wa-block').removeClass('wa-show')
   })
 
-
-  $(document).on('click', '.adaptiv__header-x', function () {
-    $('.adaptiv').slideToggle(500)
-    $('.page').slideToggle(500)
-  })
 })
